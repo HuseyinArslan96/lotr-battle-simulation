44b0418239218ad4e019ef8335b76be1905e1c10
HuseyinArslan96 <118057362+HuseyinArslan96@users.noreply.github.com>
2023-08-13 13:56:18 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ class Dusman:
         print(f"Kalan büyü sayısı: {self.buyu} ")
         verilenZarar = harcananBuyu * self.saldiriGucu
         print("Verilen zarar: ", verilenZarar)
+        
         return (harcananBuyu, self.saldiriGucu, verilenZarar)
     def saldiriyaUgra(self, harcananBuyu, saldiriGucu):
         print(f"{self.isim}: Saldırıya uğruyorum...")
@@ -37,9 +38,10 @@ class Dusman:
         print("İsim:", self.isim, "\n" "Kalan Can:", self.can, "\n" "Saldırı Gücü:", self.saldiriGucu, "\n" "Kalan Büyü Sayısı:", self.buyu)
 
 @app.route("/", methods=["GET", "POST"])
-def savaş_simülasyonu():
+def battleSimulation():
     if request.method == "POST":
-        karakter = int(request.form.get("karakter"))
+        character = int(request.form.get("character"))
+        enemy = int(request.form.get("enemy"))
             
         melkor = Dusman("Melkor", 6000, 900, 10)
         sauron = Dusman("Sauron", 2000, 300, 10)
@@ -49,51 +51,57 @@ def savaş_simülasyonu():
         gandalf = Dusman("Gandalf", 2500, 400, 10)
         elrond = Dusman("Elrond", 1500, 200, 10)
 
-        evil = [melkor, sauron, nazgul]
-        good = [manwe, gandalf, elrond]
-        if karakter == 1:
-            karakter = manwe
-        if karakter == 2:
-            karakter = gandalf
-        if karakter == 3:
-            karakter = elrond
-        if karakter == 4:
-            karakter = melkor
-        if karakter == 5:
-            karakter = sauron
-        if karakter == 6:
-            karakter = nazgul
-        if karakter in evil:
-            rastgele = random.choice(good)
-        if karakter in good:
-            rastgele = random.choice(evil)    
-        donenDeger1 = karakter.saldir()
-        rastgele.saldiriyaUgra(donenDeger1[0], donenDeger1[1])
-        rastgele.buyuBittiMi()
-        rastgele.hayattaMi()
-        donenDeger2 = rastgele.saldir()
-        karakter.saldiriyaUgra(donenDeger2[0], donenDeger2[1])
-        karakter.buyuBittiMi()
-        karakter.hayattaMi()
-        if karakter.can > rastgele.can:
-            kazanan = karakter.isim
-            kaybeden = rastgele.isim
-            saldiri1 = karakter.saldiriGucu
-            saldiri2 = rastgele.saldiriGucu
-            if rastgele.can <= 0:
-                rastgele.can = 0
-                return render_template("result.html", kazanan = kazanan, saldiri1 = saldiri1, kalanCan = karakter.can, kalanBuyu = karakter.buyu, kaybeden = kaybeden, saldiri2 = saldiri2, kaybedenCan = rastgele.can, kaybedenBuyu = rastgele.buyu)
-        elif rastgele.can > karakter.can:
-            kazanan = rastgele.isim
-            kaybeden = karakter.isim
-            saldiri1 = rastgele.saldiriGucu
-            saldiri2 = karakter.saldiriGucu
-            if karakter.can <= 0:
-                karakter.can = 0
-                return render_template("result.html", kazanan = kazanan, saldiri1 = saldiri1, kalanCan = rastgele.can, kalanBuyu = rastgele.buyu, kaybeden = kaybeden, saldiri2 = saldiri2, kaybedenCan = karakter.can, kaybedenBuyu = karakter.buyu)
+        if character == 1:
+            character = manwe
+        if character == 2:
+            character = gandalf
+        if character == 3:
+            character = elrond
+        if character == 4:
+            character = melkor
+        if character == 5:
+            character = sauron
+        if character == 6:
+            character = nazgul
+        if enemy == 1:
+            enemy = manwe
+        if enemy == 2:
+            enemy = gandalf
+        if enemy == 3:
+            enemy = elrond
+        if enemy == 4:
+            enemy = melkor
+        if enemy == 5:
+            enemy = sauron
+        if enemy == 6:
+            enemy = nazgul
+        donenDeger1 = character.saldir()
+        enemy.saldiriyaUgra(donenDeger1[0], donenDeger1[1])
+        enemy.buyuBittiMi()
+        enemy.hayattaMi()
+        donenDeger2 = enemy.saldir()
+        character.saldiriyaUgra(donenDeger2[0], donenDeger2[1])
+        character.buyuBittiMi()
+        character.hayattaMi()
+        if character.can > enemy.can:
+            winner = character.isim
+            loser = enemy.isim
+            attack1 = character.saldiriGucu
+            attack2 = enemy.saldiriGucu
+            if enemy.can <= 0:
+                enemy.can = 0
+                return render_template("result.html", winner = winner, attack1 = attack1, winnerHealth = character.can, winnerMagic = character.buyu, loser = loser, attack2 = attack2, loserHealth = enemy.can, loserMagic = enemy.buyu)
+        elif enemy.can > character.can:
+            winner = enemy.isim
+            loser = character.isim
+            attack1 = enemy.saldiriGucu
+            attack2 = character.saldiriGucu
+            if character.can <= 0:
+                character.can = 0
+                return render_template("result.html", winner = winner, attack1 = attack1, winnerHealth = enemy.can, winnerMagic = enemy.buyu, loser = loser, attack2 = attack2, loserHealth = character.can, loserMagic = character.buyu)
         else:
             print("Savaş berabere bitti.")
-    return render_template("index.html")
+    return render_template("index.html")    
 
 if __name__ == "__main__":
     app.run(debug=True)
